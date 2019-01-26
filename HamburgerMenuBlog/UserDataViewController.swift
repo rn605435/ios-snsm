d6f484940a44830072003d847870df297ca1b27a
nicolas <nicolas@nicolass-Mac.local>
2019-01-26 10:56:27 -0800

    added form, localstorage

--- HamburgerMenuBlog/UserDataViewController.swift
+++ HamburgerMenuBlog/UserDataViewController.swift
@@ -7,23 +7,40 @@
 //
 
 import UIKit
+import IQKeyboardManagerSwift
 
 class UserDataViewController: UIViewController {
 
+    @IBOutlet weak var boatName: UITextField!
+    @IBOutlet weak var boatBrand: UITextField!
+    @IBOutlet weak var boatLength: UITextField!
+    @IBOutlet weak var boatWidth: UITextField!
+    @IBOutlet weak var boatDraught: UITextField!
+    @IBOutlet weak var boatMatriculation: UITextField!
+    @IBOutlet weak var boatMarina: UITextField!
+    @IBOutlet weak var boatContact: UITextField!
+    @IBOutlet weak var boatPlace: UITextField!
+    @IBOutlet weak var boatPlaceNbr: UITextField!
+    @IBOutlet weak var boatInsurance: UITextField!
+    @IBOutlet weak var boatInsurancePolice: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        IQKeyboardManager.shared.enable = true
+
+        let savedBoatName = UserDefaults.standard.object(forKey: "BoatName")
+        if let boatname = savedBoatName as? String {
+            boatName.text = boatname
+        }
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    @IBAction func saveBtn(_ sender: UIButton) {
+        UserDefaults.standard.set(boatName.text, forKey: "BoatName")
+        
     }
-    */
-
+    @IBAction func deleteBtn(_ sender: UIButton) {
+        UserDefaults.standard.removeObject(forKey: "BoatName")
+    }
+    
+    
 }
